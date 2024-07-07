c6efcc92a406d552cd18d4aaaaf4bdc83bc4baf8
Randila Premarathne <randilamenukapremarathne@gmail.com>
2024-07-07 21:34:43 +0530

    Add tailwind and setup pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import { Router,Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import News from "./pages/News";
+import Settings from "./pages/Settings";
+import Signin from "./components/Signin/Signin";
 
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home />)
+  },
+  {
+    path: "/signin",
+    element: (<Signin />)
+  },
+  {
+    path: "/dashboard",
+    element: (<Dashboard />)
+  },
+  {
+    path: "/events",
+    element: (<Events />)
+  },
+  
+  {
+    path: "/news",
+    element: (<News />)
+  },
+  {
+    path: "/settings",
+    element: (<Settings />)
+  }
+])
 function App() {
   return (
-    <div className="App">
-      <h1>Sasnaka App</h1>
-    </div>
+    <>
+      <RouterProvider router={router} /> 
+    </>
   );
 }
 
