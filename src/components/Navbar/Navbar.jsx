ada28f126d2e3118d6d790d7b5ddaad723b4e1e0
wathsalaMadanasinghe <cwathsala123@gmail.com>
2024-07-10 23:41:07 +0530

    Setting_up_1
    ## Changes
    - The navbar is now removed from the fixed position and hidden from mobile view.
    - The settings page is developed.
    - New assets have been added to the project.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     //give me a dashboard navbar that fixed in left side with tailwind
-    <div className="bg-gray-800 h-screen w-64 fixed top-0 left-0">
+    <div className="hidden bg-gray-800 h-screen w-64 top-0 left-0">
       <div className="text-white text-2xl font-bold p-4">Dashboard</div>
-      <Link to="/dashboard" className="block text-white p-4 hover:bg-gray-700">Home</Link>
-      <Link to="/gallery" className="block text-white p-4 hover:bg-gray-700">Gallery</Link>
-      <Link to="/events" className="block text-white p-4 hover:bg-gray-700">Events</Link>
-      <Link to="/news" className="block text-white p-4 hover:bg-gray-700">News</Link>
-      <Link to="/settings" className="block text-white p-4 hover:bg-gray-700">Settings</Link>
+      <Link to="/dashboard" className="block text-white p-4 hover:bg-gray-700">
+        Home
+      </Link>
+      <Link to="/gallery" className="block text-white p-4 hover:bg-gray-700">
+        Gallery
+      </Link>
+      <Link to="/events" className="block text-white p-4 hover:bg-gray-700">
+        Events
+      </Link>
+      <Link to="/news" className="block text-white p-4 hover:bg-gray-700">
+        News
+      </Link>
+      <Link to="/settings" className="block text-white p-4 hover:bg-gray-700">
+        Settings
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,13 +1,105 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import profile from "../assets/profile.jpg";
+import qr from "../assets/QR.svg";
+import account from "../assets/account.svg";
+import security from "../assets/security.svg";
+import notification from "../assets/notification.svg";
+import display from "../assets/display.svg";
+import event from "../assets/event.svg";
+import news from "../assets/news.svg";
+import help from "../assets/help.svg";
+
+const SettingOption = ({ icon, title, description }) => {
+  return (
+    <div className="flex group lg:p-4 sm:p-0 items-center lg:ml-20 sm:ml-10 bg-white mb-6 rounded-2xl w-10/12 shadow-sm cursor-pointer hover:bg-gradient-to-tr hover:bg-[#e9e8e8]   transition duration-300">
+      <div className="p-3 rounded-full bg-none">{icon}</div>
+      <div className="ml-4 ">
+        <h3 className="text-xl font-medium font-sans">{title}</h3>
+        <p className=" text-gray-500 font-sans lg:text-lg sm:text-sm ">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 function Settings() {
   return (
-    
-    <div>
-        <Navbar />
-        Settings</div>
-  )
+    <div className=" flex h-screen bg-gray-100 overflow-auto">
+      <Navbar />
+      <div className="flex-1 ml-12">
+        <h1 className="uppercase font-bold text-3xl font-sans mt-8 ">
+          Settings
+        </h1>
+        <div className="bg-white rounded shadow-2xl mt-10 mr-10">
+          <div className="flex p-16 justify-start">
+            <img
+              src={profile}
+              className="mr-10"
+              width={180}
+              height={180}
+              alt="/"
+            />
+            <div className="lg:mr-40 sm:mr-16">
+              <h2 className="font-semibold font-sans mb-4 lg:text-3xl md:text-2xl sm:text-xl">
+                Mark Perera
+              </h2>
+              <p className="uppercase lg:text-xl font-sans text-[#0098DA] font-bold sm:text-sm">
+                volunteer lvl ii
+              </p>
+            </div>
+            <div>
+              <img
+                src={qr}
+                alt="/"
+                className="rounded-md justify-center cursor-pointer hover:bg-[#e9e8e8]"
+                width={40}
+              />
+            </div>
+          </div>
+
+          <div className="ml-4 pb-10">
+            <SettingOption
+              icon={<img src={account} alt="/" />}
+              title="Account "
+              description="Profile Information, Delete Account"
+            />
+            <SettingOption
+              icon={<img src={security} alt="/" />}
+              title="Privacy & Security"
+              description="Privacy Setting, Security Setting"
+            />
+            <SettingOption
+              icon={<img src={notification} alt="/" />}
+              title="Notification"
+              description="Email Notification, Push Notification"
+            />
+            <SettingOption
+              icon={<img src={display} alt="/" />}
+              title="Display"
+              description="Email Notification, Push Notification"
+            />
+            <SettingOption
+              icon={<img src={event} alt="/" />}
+              title="Event Preferences"
+              description="Event Registers, Event Visibility"
+            />
+            <SettingOption
+              icon={<img src={news} alt="/" />}
+              title="News Preferences"
+              description="News Subscription, News Notification Setting"
+            />
+            <SettingOption
+              icon={<img src={help} alt="/" />}
+              title="Help & Support"
+              description="Support Contact Information, FAQ & Help Center"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Settings
+export default Settings;
